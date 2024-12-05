0bd6e1c50abb932f531db2aea068690ca884bb0f
Aravind Kumar <arakumar@griddynamics.com>
2024-12-05 12:46:28 +0530

    Initialize project using Create React App

---
+++ src/components/Head.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="menu" />
+        <img src="" alt="logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
